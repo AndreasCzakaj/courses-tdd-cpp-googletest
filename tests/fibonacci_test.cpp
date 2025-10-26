fce591a6d7ea3d1dd2f58a8ff407bf5208be6381
Andreas Czakaj <andreas.czakaj@binary-stars.eu>
2025-10-26 18:11:33 +0100

    add EXPECT_THAT

--- tests/fibonacci_test.cpp
+++ tests/fibonacci_test.cpp
@@ -1,5 +1,10 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 #include "fibonacci.h"
+
+using ::testing::IsEmpty;
+using ::testing::Not;
+using ::testing::Eq;
 
 // https://www.wackerart.de/mathematik/big_numbers/fibonacci_numbers.html
 
@@ -7,23 +12,24 @@
 // Basic test - this will fail until implemented
 // ============================================================================
 
-TEST(FibonacciTest,  DISABLED_ReturnZeroForIndexZero) {
+TEST(FibonacciTest,  /* DISABLED_ */ReturnZeroForIndexZero) {
     // given / Arrange
     Fibonacci fib;
     int index = 0;
-
+    
     // when / Act
     auto actual = fib.calc(index);
-
+    
     // then / Assert
-    int expected = 0;
-    EXPECT_EQ(actual, expected) << "It should yield zero for index zero";
-
+    int expected = 1;
+    EXPECT_EQ(actual, expected) << "It should yield zero for index zero I";
+    EXPECT_THAT(actual, Eq(expected)) << "It should yield zero for index zero II";
+    
     // this is the same but compacted:
-    EXPECT_EQ(fib.calc(0), 0) << "It should yield zero for index zero";
-
+    EXPECT_EQ(fib.calc(0), 0) << "It should yield zero for index zero III";
+    
     // this is the same but different: will STOP the test on failure:
-    ASSERT_EQ(fib.calc(0), 0) << "It should yield zero for index zero";
+    ASSERT_EQ(fib.calc(0), 0) << "It should yield zero for index zero IV";
 }
 
 TEST(FibonacciTest, DISABLED_ReturnsOneForIndexOne) {
